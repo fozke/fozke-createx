e5b82b6c41f84492bedc018f5818d40459bcb685
fozke <118976821+fozke@users.noreply.github.com>
2022-12-03 00:46:56 +0300

    createx

--- js/main.js
+++ js/main.js
@@ -7,4 +7,20 @@ $(function () {
     $(this).addClass('directions__filter-btn--active')
   })
 
+  $('.team__slider').slick({
+    arrows: false,
+    slidesToShow: 4,
+    infinite: true,
+    draggable: false,
+  })
+  $('.team__slider-prev').on('click', function (e) {
+    e.preventDefault()
+    $('.team_slider').slick('slickPrev')
+  })
+  $('.team__slider-next').on('click', function (e) {
+    e.preventDefault()
+    $('.team_slider').slick('slickNext')
+  })
+
+
 });
